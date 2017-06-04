fe911d99f1639b446957a8fcef11ef283cc9ecec
Misheca <Misheca>
2017-06-04 13:13:20 -0500

    update interval

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var numeroSlide = 1;
+var resetloop = true;
 //indicadores
 $("#indicator li").click(function () {
   var roleStyle = $(this).attr("role-slide");
@@ -7,9 +8,11 @@ $("#indicator li").click(function () {
   $("#slide ul li:nth-child("+roleStyle+")").fadeIn();
   $("#indicator li").css({"opacity":"0.5"});
   $(this).css({"opacity":"2.5"});
+  resetloop = true;
+  numeroSlide = roleStyle
 });
-//flechas
-$("#right").click(function () {
+//global loop
+function loop() {
   if(numeroSlide >= 5){
     numeroSlide=1;
   }else {
@@ -19,7 +22,13 @@ $("#right").click(function () {
   $("#slide ul li:nth-child("+numeroSlide+")").fadeIn();
   $("#indicator li").css({"opacity":"0.5"});
   $("#indicator li:nth-child("+numeroSlide+")").css({"opacity":"2.5"});
-});
+}
+
+//flechas
+$("#right").click(function () {
+  loop();
+  resetloop = true;
+  });
 
 $("#left").click(function () {
   if(numeroSlide <= 1){
@@ -31,4 +40,15 @@ $("#left").click(function () {
   $("#slide ul li:nth-child("+numeroSlide+")").fadeIn();
   $("#indicator li").css({"opacity":"0.5"});
   $("#indicator li:nth-child("+numeroSlide+")").css({"opacity":"2.5"});
+  resetloop = true;
 });
+//loop slide
+setInterval(function () {
+  if(resetloop){
+    resetloop=false;
+  }
+  else {
+    loop();
+  }
+
+},3000)
